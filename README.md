c92262680269ebb5a1ceaec70d9436ada5c91e90
Or Hasson <hassonor@gmail.com>
2024-12-18 18:51:32 +0200

    set basic kafka setup

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 1. TODO: Create shared package
 2. TODO: Secure the communication between microservices
 3. TODO: Implement a real autMiddleware for cart and order routes
+4. TODO: Add redpanda to watch kafka data
+5. TODO: Improve Kafka configuration
+6. 
--- order_service/src/express-app.ts
+++ order_service/src/express-app.ts
@@ -2,7 +2,9 @@ import express, {NextFunction, Request, Response} from "express";
 import cors from "cors";
 import orderRoutes from "./routes/order.routes";
 import cartRoutes from "./routes/cart.routes";
-import {HandleErrorWithLogger, httpLogger} from "./utils";
+import {HandleErrorWithLogger, httpLogger, logger} from "./utils";
+import {MessageBroker} from "./utils";
+import {Consumer, Producer} from "kafkajs";
 
 
 export const ExpressApp = async () => {
@@ -10,6 +12,24 @@ export const ExpressApp = async () => {
     app.use(cors());
     app.use(express.json());
     app.use(httpLogger);
+
+    // 1st step: connect to the producer and consumer
+    const producer = await MessageBroker.connectProducer<Producer>();
+    producer.on("producer.connect", () => {
+        logger.info("producer connected");
+    })
+
+    const consumer = await MessageBroker.connectConsumer<Consumer>();
+    consumer.on("consumer.connect", () => {
+        logger.info("consumer connected");
+    })
+
+    // 2nd step: subscribe to the topic or publish the message
+    await MessageBroker.subscribe((message) => {
+        logger.info("Consumer received the message");
+        console.log("Message received", message);
+
+    }, "OrderEvents");
 
     app.use(orderRoutes)
     app.use(cartRoutes);
